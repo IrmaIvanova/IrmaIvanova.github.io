cefd662a98182a5250208e2aecf9930054d14cb7
Irma Ivanova <nowa.mir@yandex.ru>
2019-01-30 16:25:11 +0300

    first commit

---
+++ img/venice/batch-image-resize.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Purpose: batch image resizer
+# Source: https://guides.wp-bullet.com
+# Author: Mike
+
+# absolute path to image folder
+FOLDER="."
+
+# max height
+WIDTH=600
+
+# max width
+HEIGHT=400
+
+#resize png or jpg to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.jpg' -o -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize png to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize jpg only to either height or width, keeps proportions using imagemagick
+find ${FOLDER} -iname '*.jpg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+# alternative
+#mogrify -path ${FOLDER} -resize ${WIDTH}x${HEIGHT}% *.png -verbos.
